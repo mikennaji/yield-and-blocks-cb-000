defb001f03ec45bb0903b92384042c2204f74dc7
Michael Nnaji <mnnaji3@uwo.ca>
2020-04-17 14:26:31 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,21 @@
 def hello_t(array)
-  i = 0
-  if array.size > 1
+  
+  if block_given?
+    i = 0
+    
     while i < array.size
+
       yield array[i]
       i = i +1
+
     end
 
   elsif array.size == 0
+
     puts '"Hey! No block was given!\n"'
+
   end
+
   return array
 
 end
